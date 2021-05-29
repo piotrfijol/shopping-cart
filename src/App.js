9f807ce01ce9694cae4cb05108c89d5cb86c4398
Piotr Fijol <piotr.fijol1@gmail.com>
2021-05-29 18:31:01 +0200

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 
+import './styles/style.css';
+
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <div className="navbar">
+        <ul>
+          <li><a href="" className="navbar__link"><i class="fas fa-home"></i></a></li>
+          <li><a href="" className="navbar__link"><i class="fas fa-info-circle"></i></a></li>
+          <li><a href="" className="navbar__link"><i class="fas fa-shopping-cart"></i></a></li>
+          <li>
+            <div className="navbar__link__burger">
+              <input id="burger-menu" type="checkbox"/>
+              <label htmlFor="burger-menu"></label>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
